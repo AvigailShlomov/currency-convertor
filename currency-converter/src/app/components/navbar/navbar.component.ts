d7f8a06797027e8220e5a2332200d6ab9326e25c
AvigailShlomov <anm798@gmail.com>
2025-03-30 21:31:09 +0300

    fix: add RouterLink import to resolve navigation issue

--- currency-converter/src/app/components/navbar/navbar.component.ts
+++ currency-converter/src/app/components/navbar/navbar.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { RouterLink } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-navbar',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLink],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
-export class NavbarComponent {
-
-}
+export class NavbarComponent {}
