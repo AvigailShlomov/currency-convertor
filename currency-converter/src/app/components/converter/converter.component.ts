a3a1bf572e5abd8c1bd589f0aee2f53da23c42cb
AvigailShlomov <anm798@gmail.com>
2025-03-30 17:38:10 +0300

    feat: add loadCurrencies method to ConverterComponent

--- currency-converter/src/app/components/converter/converter.component.ts
+++ currency-converter/src/app/components/converter/converter.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import {
+  FormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { ConverterService } from '../../services/converter.service';
 
 @Component({
   selector: 'app-converter',
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatSelectModule, MatInputModule],
   templateUrl: './converter.component.html',
-  styleUrl: './converter.component.css'
+  styleUrl: './converter.component.css',
 })
-export class ConverterComponent {
+export class ConverterComponent implements OnInit {
+  converterService = inject(ConverterService);
 
+  convertForm = new FormGroup({
+    amout: new FormControl<number>(1, Validators.required),
+    from: new FormControl<string>('USD', Validators.required),
+    to: new FormControl<string>('ILS', Validators.required),
+  });
+  currencies = signal<[string, string][]>([]);
+
+  ngOnInit() {
+    this.loadCurrencies();
+  }
+
+  loadCurrencies() {
+    this.converterService.getCurrencies().subscribe({
+      next: (currencies) => {
+        this.currencies.set(Object.entries(currencies));
+      },
+      error: (err) => alert('Error while fetching currencies list'),
+    });
+  }
 }
