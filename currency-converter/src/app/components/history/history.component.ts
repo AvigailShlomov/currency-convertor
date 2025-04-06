9a74fad50fdef107892ba362fc723ab7cf7e765f
AvigailShlomov <anm798@gmail.com>
2025-04-07 00:40:40 +0300

    feat(history): add logic to historyComponent for the table+ getConversionHistory method

--- currency-converter/src/app/components/history/history.component.ts
+++ currency-converter/src/app/components/history/history.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  computed,
+  inject,
+  OnInit,
+  signal,
+  ViewChild,
+} from '@angular/core';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { DatePipe, DecimalPipe } from '@angular/common';
+import { StorageService } from '../../services/storage.service';
+import { ConversionStorage } from '../../models/currency.models';
+import { STORAGE_KEYS } from '../../config/app.constants';
 
 @Component({
   selector: 'app-history',
-  imports: [],
+  imports: [MatTableModule, MatPaginatorModule, DecimalPipe, DatePipe],
   templateUrl: './history.component.html',
-  styleUrl: './history.component.css'
+  styleUrl: './history.component.css',
 })
-export class HistoryComponent {
+export class HistoryComponent implements OnInit {
+  storageService = inject(StorageService);
 
+  displayedColumns: string[] = ['amount', 'from', 'to', 'result', 'date'];
+  ELEMENT_DATA = signal<ConversionStorage[]>([]);
+  dataSource = computed(() => {
+    return new MatTableDataSource<ConversionStorage>(this.ELEMENT_DATA());
+  });
+  @ViewChild(MatPaginator)
+  paginator: MatPaginator = new MatPaginator();
+
+  ngOnInit(): void {
+    this.ELEMENT_DATA.set(this.getConversionHistory());
+  }
+
+  getConversionHistory(): ConversionStorage[] {
+    const conversionHistory = this.storageService.getItem(STORAGE_KEYS.HISTORY);
+    if (conversionHistory) {
+      return conversionHistory as ConversionStorage[];
+    }
+    return [];
+  }
+
+  ngAfterViewInit() {
+    this.dataSource().paginator = this.paginator;
+  }
 }
