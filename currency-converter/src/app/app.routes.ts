cace9269d5918830e94db46ad5e1c9f99e0ae469
AvigailShlomov <anm798@gmail.com>
2025-08-04 16:46:54 +0300

    feat(routes): add chart route

--- currency-converter/src/app/app.routes.ts
+++ currency-converter/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { ConverterComponent } from './components/converter/converter.component';
 import { HistoryComponent } from './components/history/history.component';
+import { ChartComponent } from './components/chart/chart.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path:'history',
         component:HistoryComponent /**@todo: implamemt lazy loading */
+    },
+    {
+        path:'chart',
+        component:ChartComponent /**@todo: implamemt lazy loading */
     }
 ];
